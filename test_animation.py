c7dc04e9e33c5af52b5eb630b110e81921102845
M1naR0blox <144395767+M1naR0blox@users.noreply.github.com>
2023-11-08 16:53:23 -0600

    added 1 new animation and updated an existing one

--- test_animation.py
+++ test_animation.py
@@ -7,54 +7,41 @@ def clear():
 def sleep(num):
     time.sleep(num)
 
-def loading_animation():
-    print("Loading")
-    sleep(0.2)
-    clear()
-    print("Loading.")
-    sleep(0.2)
-    clear()
-    print("Loading..")
-    sleep(0.2)
-    clear()
-    print("Loading...")
-    sleep(0.2)
-    clear()
-    print("Loading")
-    sleep(0.2)
-    clear()
-    print("Loading.")
-    sleep(0.2)
-    clear()
-    print("Loading..")
-    sleep(0.2)
-    clear()
-    print("Loading...")
-    sleep(0.2)
-    clear()
-    print("Loading")
-    sleep(0.2)
-    clear()
-    print("Loading.")
-    sleep(0.2)
-    clear()
-    print("Loading..")
-    sleep(0.2)
-    clear()
-    print("Loading...")
-    sleep(0.2)
-    clear()
-    print("Loading")
-    sleep(0.2)
-    clear()
-    print("Loading.")
-    sleep(0.2)
-    clear()
-    print("Loading..")
-    sleep(0.2)
-    clear()
-    print("Loading...")
-    sleep(0.2)
-    clear()
+def loading_animation(times_repeated):
+    acum = 0
+    while True:
+        print("Loading")
+        sleep(0.2)
+        clear()
+        print("Loading.")
+        sleep(0.2)
+        clear()
+        print("Loading..")
+        sleep(0.2)
+        clear()
+        print("Loading...")
+        sleep(0.2)
+        clear()
+        acum += 1
+        if acum == times_repeated:
+            break
 
 
+def custom_loading_animation(text_for_loading, times_repeated):
+    x = 0
+    while True:
+        print(f"{text_for_loading}")
+        sleep(0.2)
+        clear()
+        print(f"{text_for_loading}.")
+        sleep(0.2)
+        clear()
+        print(f"{text_for_loading}..")
+        sleep(0.2)
+        clear()
+        print(f"{text_for_loading}...")
+        sleep(0.2)
+        clear()
+        x += 1
+        if x == times_repeated:
+            break
