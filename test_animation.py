2c7308e82351dbbbf40efcfc3ba000759a3063db
M1naR0blox <144395767+M1naR0blox@users.noreply.github.com>
2023-11-07 21:00:37 -0600

    another project

---
+++ test_animation.py
@@ -0,0 +1,11 @@
+import time
+import os
+
+def clear():
+    os.system("cls")
+
+def sleep(num):
+    time.sleep(num)
+
+def test_animation():
+    print("For now, this isnt availible but soon will")
